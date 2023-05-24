9bb8230ddf7498cb36f7ddf1d1cc03a5441a3e5c
Doha0 <abu.jardoha@gmail.com>
2023-05-25 00:45:08 +0600

    navbar and banner added

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { router } from './Routes/Routes.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <div className='container mx-auto px-4'>
+    <div className='container mx-auto px-4 lg:p-0'>
       <RouterProvider router={router} />
     </div>
   </React.StrictMode>,
