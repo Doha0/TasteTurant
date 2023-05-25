6e1f37324209bb2890631bbe8c067ae10241d93f
Doha0 <abu.jardoha@gmail.com>
2023-05-25 17:52:29 +0600

    menu added

---
+++ src/Pages/MenuPage/Menu/Menu.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Helmet } from 'react-helmet-async';
+import Cover from '../../Shared/Cover/Cover';
+import menuImg from '../../../assets/menu/banner3.jpg'
+import PopularMenu from '../../HomePage/PopularMenu/PopularMenu';
+
+const Menu = () => {
+    return (
+        <div>
+            <Helmet>
+                <title>TasteTurant | Menu</title>
+            </Helmet>
+            <Cover img={menuImg} title="our menu"></Cover>
+            <PopularMenu></PopularMenu>
+            <Cover img={menuImg} title="our menu"></Cover>
+            <PopularMenu></PopularMenu>
+            <Cover img={menuImg} title="our menu"></Cover>
+            <PopularMenu></PopularMenu>
+        </div>
+    );
+};
+
+export default Menu;
--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,14 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import { router } from './Routes/Routes.jsx';
+import { HelmetProvider } from 'react-helmet-async';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <div className='container mx-auto px-4 lg:p-0'>
-      <RouterProvider router={router} />
-    </div>
+    <HelmetProvider>
+      <div className='container mx-auto px-4 lg:p-0'>
+        <RouterProvider router={router} />
+      </div>
+    </HelmetProvider>
   </React.StrictMode>,
 )
