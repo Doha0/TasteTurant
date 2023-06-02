7a4768d64447c759a15db472dfb9451483b53228
Doha0 <abu.jardoha@gmail.com>
2023-06-02 16:05:34 +0600

    user data load by tanstack query

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
-import { FaHome, FaCalendarAlt, FaWallet, FaShoppingCart } from 'react-icons/fa';
+import { FaHome, FaCalendarAlt, FaWallet, FaShoppingCart, FaUtensils, FaBook, FaUsers } from 'react-icons/fa';
 import useCart from '../Hooks/useCart';
 
 const Dashboard = () => {
     const [cart] = useCart();
+
+    // TODO: load data from the server to have dynamic isAdmin based on Data
+    const isAdmin = true;
+
     return (
         <div>
             <div className="drawer drawer-mobile">
@@ -19,15 +23,27 @@ const Dashboard = () => {
                     <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
                     <ul className="menu p-4 w-80 ">
                         {/* <!-- Sidebar content here --> */}
-                        <li><NavLink to="/dashboard/home"><FaHome></FaHome> User Home</NavLink></li>
-                        <li><NavLink to="/dashboard/reservations"><FaCalendarAlt></FaCalendarAlt> Reservations</NavLink></li>
-                        <li><NavLink to="/dashboard/history"><FaWallet></FaWallet> Payment History</NavLink></li>
-                        <li>
-                            <NavLink to="/dashboard/mycart"><FaShoppingCart></FaShoppingCart> My Cart
-                                <span className="badge inl badge-secondary">+{cart?.length || 0}</span>
-                            </NavLink>
+                        {
+                            isAdmin ? <>
+                                <li><NavLink to="/dashboard/home"><FaHome></FaHome> Admin Home</NavLink></li>
+                                <li><NavLink to="/dashboard/reservations"> <FaUtensils></FaUtensils> Add Items</NavLink></li>
+                                <li><NavLink to="/dashboard/history"><FaWallet></FaWallet> Manage Items</NavLink></li>
+                                <li><NavLink to="/dashboard/history"><FaBook></FaBook> Manage Bookings</NavLink></li>
+                                <li><NavLink to="/dashboard/allusers"><FaUsers></FaUsers> All Users</NavLink></li>
 
-                        </li>
+                            </> : <>
+                                <li><NavLink to="/dashboard/home"><FaHome></FaHome> User Home</NavLink></li>
+                                <li><NavLink to="/dashboard/reservations"><FaCalendarAlt></FaCalendarAlt> Reservations</NavLink></li>
+                                <li><NavLink to="/dashboard/history"><FaWallet></FaWallet> Payment History</NavLink></li>
+                                <li>
+                                    <NavLink to="/dashboard/mycart"><FaShoppingCart></FaShoppingCart> My Cart
+                                        <span className="badge inl badge-secondary">+{cart?.length || 0}</span>
+                                    </NavLink>
+
+                                </li>
+                            </>
+                        }
+
 
                         <div className="divider"></div>
                         <li><NavLink to="/"><FaHome></FaHome> Home</NavLink> </li>
