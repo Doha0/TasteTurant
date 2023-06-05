cca76578a816b961496b3a40206972445c587be4
Doha0 <abu.jardoha@gmail.com>
2023-06-05 08:04:12 +0600

    food item added

--- src/Pages/Dashboard/AddItems/AddItems.jsx
+++ src/Pages/Dashboard/AddItems/AddItems.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import SectionTitle from '../../../Components/SectionTitle/SectionTitle';
 import { useForm } from 'react-hook-form';
+import useAxiosSecure from '../../../Hooks/useAxiosSecure';
 
 const img_hosting_token = import.meta.env.VITE_Image_Upload_Token;
 
 const AddItems = () => {
-
+    const [axiosSecure] = useAxiosSecure();
     const { register, handleSubmit, formState: { errors } } = useForm();
     const img_hosting_url = `https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_Image_Upload_Token}`;
 
@@ -19,11 +20,15 @@ const AddItems = () => {
         })
             .then(res => res.json())
             .then(imgResponse => {
-                if(imgResponse.success){
+                if (imgResponse.success) {
                     const imgURL = imgResponse.data.display_url;
-                    const {name, price, category, recipe} = data;
-                    const newItem = {name, price: parseFloat(price), category, recipe, image: imgURL};
+                    const { name, price, category, recipe } = data;
+                    const newItem = { name, price: parseFloat(price), category, recipe, image: imgURL };
                     console.log(newItem);
+                    axiosSecure.post('/menu', newItem)
+                        .then(data =>{
+                            console.log('after posting new menu item', data.data)
+                        })
                 }
             })
     };
